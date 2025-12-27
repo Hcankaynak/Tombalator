5c0d5b0b4619dab488325c545a2574ed82e1d29d
hcankaynak <h.cankaynak@gmail.com>
2025-12-27 12:16:19 +0300

    initial commit

---
+++ server/src/main/kotlin/Application.kt
@@ -0,0 +1,28 @@
+package com.tombalator
+
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+            ignoreUnknownKeys = true
+        })
+    }
+    
+    install(CORS) {
+        anyHost()
+        allowHeader("Content-Type")
+    }
+    
+    configureRouting()
+}
